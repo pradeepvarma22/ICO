e9949a0fbfb56951afee4be381b0767cfebf59b6
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-09-02 19:05:49 +0530

    app: tailwindcss
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

---
+++ ico-app/pages/components/Header/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Header() {
+    return (
+        <div>
+            <Head>
+                <title>ICO</title>
+                <meta name="description" content="ICO" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </div>
+    );
+}
